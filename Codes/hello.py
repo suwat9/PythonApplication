5c2c31307031ee6119244fac3c37c884215481b1
Suwat <suwat.tac@mail.pbru.ac.th>
2023-01-31 15:26:21 +0700

    Update hello.py

--- Codes/hello.py
+++ Codes/hello.py
@@ -3,9 +3,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():    
  return render_template('index.html')
-@app.route('/user/<name>')
-def user(name):
- return render_template('user.html', name=name)
+
+@app.route('/user')
+def user():
+ return render_template('user.html', name="Suwat",lastname="Tacha",email="Suwat.tac@mail.pbru.ac.th")
 
 if __name__ == '__main__':
     app.run()
